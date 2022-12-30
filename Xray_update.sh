2a6cc0361329252e9023937d5c1d79600676c01b
tszho-t <70687638+tszho-t@users.noreply.github.com>
2022-12-30 12:26:14 +0800

    Add files via upload

---
+++ Xray_update.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/env bash
+
+sed -i "/ConnetionConfig/{s/ConnetionConfig/ConnectionConfig/g}" /etc/XrayR/config.yml
+
+cd
+wget https://raw.githubusercontent.com/XrayR-project/XrayR-release/master/XrayR.sh && chmod +x && ./XrayR.sh update
+
+rm -rf XrayR.sh
