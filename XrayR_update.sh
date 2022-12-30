d19b69fb52b2e95ff4560886a9869261e968f1d7
tszho-t <70687638+tszho-t@users.noreply.github.com>
2022-12-30 12:29:02 +0800

    Update XrayR_update.sh

--- XrayR_update.sh
+++ XrayR_update.sh
@@ -3,6 +3,6 @@
 sed -i "/ConnetionConfig/{s/ConnetionConfig/ConnectionConfig/g}" /etc/XrayR/config.yml
 
 cd
-wget https://raw.githubusercontent.com/XrayR-project/XrayR-release/master/XrayR.sh && chmod +x && ./XrayR.sh update
+wget https://raw.githubusercontent.com/XrayR-project/XrayR-release/master/XrayR.sh && chmod +x XrayR.sh && ./XrayR.sh update
 
 rm -rf XrayR.sh
