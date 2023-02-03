03c88a7d9c439453787311b53a4e97792b8533c0
tszho-t <70687638+tszho-t@users.noreply.github.com>
2023-02-03 14:25:23 +0800

    Update XrayR_update.sh

--- XrayR_update.sh
+++ XrayR_update.sh
@@ -1,6 +1,8 @@
 #!/usr/bin/env bash
 cd
 sed -i "/ConnetionConfig/{s/ConnetionConfig/ConnectionConfig/g}" /etc/XrayR/config.yml
+sed -i "/PanelType/{s/\"V2board\"/\"NewV2board\"/g}" /etc/XrayR/config.yml
+
 wget https://raw.githubusercontent.com/XrayR-project/XrayR-release/master/XrayR.sh
 chmod +x XrayR.sh
 ./XrayR.sh update
